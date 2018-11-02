3ec7f5c63e88c60d98557ae5435dc8db602b93e7
hifly81 <gio.marigi@gmail.com>
2018-11-02 18:24:56 +0100

    format and indent

--- kafka-producer/src/main/java/com/redhat/kafka/demo/producer/serializer/perspicuus/AvroSerializer.java
+++ kafka-producer/src/main/java/com/redhat/kafka/demo/producer/serializer/perspicuus/AvroSerializer.java
@@ -1,11 +1,5 @@
 package com.redhat.kafka.demo.producer.serializer.perspicuus;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Map;
-
-import javax.xml.bind.DatatypeConverter;
-
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.BinaryEncoder;
@@ -14,48 +8,42 @@ import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
 
 public class AvroSerializer<T extends SpecificRecordBase> implements Serializer<T> {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(AvroSerializer.class);
 
-  @Override
-  public void close() {
-    // No-op
-  }
+    @Override
+    public void close() { }
 
-  @Override
-  public void configure(Map<String, ?> arg0, boolean arg1) {
-    // No-op
-  }
+    @Override
+    public void configure(Map<String, ?> arg0, boolean arg1) { }
 
-  @Override
-  public byte[] serialize(String topic, T data) {
-    try {
-      byte[] result = null;
+    @Override
+    public byte[] serialize(String topic, T data) {
+        try {
+            byte[] result = null;
 
-      if (data != null) {
-        LOGGER.debug("data='{}'", data);
+            if (data != null) {
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        BinaryEncoder binaryEncoder =
-            EncoderFactory.get().binaryEncoder(byteArrayOutputStream, null);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                BinaryEncoder binaryEncoder =
+                        EncoderFactory.get().binaryEncoder(byteArrayOutputStream, null);
 
-        DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(data.getSchema());
-        datumWriter.write(data, binaryEncoder);
+                DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(data.getSchema());
+                datumWriter.write(data, binaryEncoder);
 
-        binaryEncoder.flush();
-        byteArrayOutputStream.close();
+                binaryEncoder.flush();
+                byteArrayOutputStream.close();
 
-        result = byteArrayOutputStream.toByteArray();
-        LOGGER.debug("serialized data='{}'", DatatypeConverter.printHexBinary(result));
-      }
-      return result;
-    } catch (IOException ex) {
-      throw new SerializationException(
-          "Can't serialize data='" + data + "' for topic='" + topic + "'", ex);
+                result = byteArrayOutputStream.toByteArray();
+            }
+            return result;
+        } catch (IOException ex) {
+            throw new SerializationException("Can't serialize data='" + data + "' for topic='" + topic + "'", ex);
+        }
     }
-  }
 }
