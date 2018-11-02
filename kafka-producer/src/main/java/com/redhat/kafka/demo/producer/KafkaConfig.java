184c7211cf4ce7575b22adf23683f94d21dad656
hifly81 <gio.marigi@gmail.com>
2018-11-02 18:28:20 +0100

    added list of brokers

--- kafka-producer/src/main/java/com/redhat/kafka/demo/producer/KafkaConfig.java
+++ kafka-producer/src/main/java/com/redhat/kafka/demo/producer/KafkaConfig.java
@@ -14,7 +14,7 @@ public class KafkaConfig {
 
     public static Properties jsonProducer() {
         Properties producerProperties = new Properties();
-        producerProperties.put("bootstrap.servers", "localhost:9092");
+        producerProperties.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
         producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         producerProperties.put("value.serializer", "com.redhat.kafka.demo.producer.serializer.json.JsonSerializer");
         return producerProperties;
@@ -22,7 +22,7 @@ public class KafkaConfig {
 
     public static Properties avroProducer() {
         Properties producerProperties = new Properties();
-        producerProperties.put("bootstrap.servers", "localhost:9092");
+        producerProperties.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
         producerProperties.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
         producerProperties.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
         producerProperties.put("schema.registry.url", "http://localhost:8081");
@@ -31,7 +31,7 @@ public class KafkaConfig {
 
     public static Properties avroPerspicuusProducer() {
         Properties producerProperties = new Properties();
-        producerProperties.put("bootstrap.servers", "localhost:9092");
+        producerProperties.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
         producerProperties.put("key.serializer", "com.redhat.kafka.demo.producer.serializer.perspicuus.AvroSerializer");
         producerProperties.put("value.serializer", "com.redhat.kafka.demo.producer.serializer.perspicuus.AvroSerializer");
         return producerProperties;
