77009e4cd3d27f67bbfa5f69adcce089e9998cf8
hifly81 <gio.marigi@gmail.com>
2019-03-05 22:43:09 +0100

    removed order-sample app

--- order-sample/order-process-service/src/main/java/com/redhat/kafka/demo/consumer/handle/ConsumerHandle.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.kafka.demo.consumer.handle;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public abstract class ConsumerHandle {
-
-    public abstract void process(ConsumerRecord record);
-}
--- order-sample/shipment-service/src/main/java/com/redhat/kafka/shipment/store/ShipmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.kafka.shipment.store;
-
-import com.redhat.kafka.shipment.model.Shipment;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ShipmentRepository extends PagingAndSortingRepository<Shipment, Long> {
-}
