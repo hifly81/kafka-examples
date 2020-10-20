d05b34d2f6d48680850a1b7a2076d6c59b02f701
hifly81 <gio.marigi@gmail.com>
2020-10-20 14:11:22 +0200

    upgrade spring boot to 2.3.x

--- kafka-springboot-consumer/src/main/java/com/redhat/demo/kafka/KafkaApplication.java
+++ kafka-springboot-consumer/src/main/java/com/redhat/demo/kafka/KafkaApplication.java
@@ -2,31 +2,12 @@ package com.redhat.demo.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @SpringBootApplication
 public class KafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
-	}
-
-	@Bean
-	public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory, MongoMappingContext context) {
-
-		MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
-		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
-
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
-
-		return mongoTemplate;
-
 	}
 
 }
