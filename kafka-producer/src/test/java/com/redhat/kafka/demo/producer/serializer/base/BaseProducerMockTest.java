910561c0b665a388f805a5c4babf088248068ab3
hifly81 <gio.marigi@gmail.com>
2020-12-23 11:22:14 +0100

    solved https://github.com/hifly81/kafka-play/issues/1

--- kafka-producer/src/test/java/com/redhat/kafka/demo/producer/serializer/base/BaseProducerMockTest.java
+++ kafka-producer/src/test/java/com/redhat/kafka/demo/producer/serializer/base/BaseProducerMockTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
+import com.redhat.kafka.demo.producer.BaseProducerCallback;
+
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -33,6 +35,32 @@ public class BaseProducerMockTest {
         final ProducerRecord<String, String> producerRecord2 = new ProducerRecord<>("topicOut", null, "test");
         baseProducer.produceFireAndForget(producerRecord1);
         baseProducer.produceFireAndForget(producerRecord2);
+
+        final List<KeyValue<String, String>> expectedList = Arrays.asList(KeyValue.pair("foo", "bar"),
+            KeyValue.pair(null,"test"));
+
+        final List<KeyValue<String, String>> actualList = mockProducer.history().stream().map(this::toKeyValue).collect(Collectors.toList());
+
+        assertThat(actualList, equalTo(expectedList));
+        
+    }
+
+    @Test
+    public void testProduceAsync() throws IOException {
+        BaseProducer baseProducer = new BaseProducer();
+        final Properties props = new Properties();
+
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("acks", "all");
+
+        final MockProducer<String, String> mockProducer = new MockProducer<>(true, new StringSerializer(), new StringSerializer());
+        baseProducer.start(mockProducer);
+
+        final ProducerRecord<String, String> producerRecord1 = new ProducerRecord<>("topicOut", "foo", "bar");
+        final ProducerRecord<String, String> producerRecord2 = new ProducerRecord<>("topicOut", null, "test");
+        baseProducer.produceAsync(producerRecord1, new BaseProducerCallback());
+        baseProducer.produceAsync(producerRecord2, new BaseProducerCallback());
 
         final List<KeyValue<String, String>> expectedList = Arrays.asList(KeyValue.pair("foo", "bar"),
             KeyValue.pair(null,"test"));
