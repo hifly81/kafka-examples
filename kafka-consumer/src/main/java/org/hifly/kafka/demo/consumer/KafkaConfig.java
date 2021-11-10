a07995ca3bfda550f3f852a49400e3b2b7890ccd
hifly81 <gio.marigi@gmail.com>
2021-11-10 10:20:11 +0100

    added method to read consumer properties from file

--- kafka-consumer/src/main/java/org/hifly/kafka/demo/consumer/KafkaConfig.java
+++ kafka-consumer/src/main/java/org/hifly/kafka/demo/consumer/KafkaConfig.java
@@ -1,11 +1,30 @@
 package org.hifly.kafka.demo.consumer;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class KafkaConfig {
 
     private static final String BROKER_LIST =
             System.getenv("kafka.broker.list") != null? System.getenv("kafka.broker.list") :"localhost:9092,localhost:9093,localhost:9094";
+
+    public static Properties loadConfig(final String configFile) throws IOException {
+        if (!Files.exists(Paths.get(configFile))) {
+            throw new IOException(configFile + " not found.");
+        }
+        final Properties cfg = new Properties();
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            cfg.load(inputStream);
+        }
+
+        cfg.put("acks", "all");
+
+        return cfg;
+    }
 
 
     public static Properties baseConsumerConfig(String groupId, String valueSerializerClassName, boolean autoCommit) {
