5719b67a01041dabfe1904785e8d695a72bac458
hifly81 <gio.marigi@gmail.com>
2022-10-10 20:47:35 +0200

    admin client added operations

--- admin-client/src/main/java/org/hifly/kafka/admin/AdminClientWrapper.java
+++ admin-client/src/main/java/org/hifly/kafka/admin/AdminClientWrapper.java
@@ -2,6 +2,8 @@ package org.hifly.kafka.admin;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.DescribeClusterResult;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.TopicListing;
 import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.Node;
 
@@ -13,6 +15,7 @@ import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class AdminClientWrapper {
 
@@ -21,6 +24,36 @@ public class AdminClientWrapper {
         String configFile = "admin.properties";
         if(args.length == 1)
             configFile = args[0];
+
+        String[] options = {
+                "1 - Nodes listing",
+                "2 - Topics listing",
+                "3 - Exit",
+        };
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        while (true) {
+            mainMenu(options);
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    nodesListing(configFile);
+                    break;
+                case 2:
+                    topicsListing(configFile);
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid operation!");
+                    break;
+            }
+        }
+
+
+    }
+
+    private static void nodesListing(String configFile) {
         try (AdminClient client = AdminClient.create(loadConfig(configFile))) {
             DescribeClusterResult describeClusterResult = client.describeCluster();
             KafkaFuture<Collection<Node>> nodesFuture = describeClusterResult.nodes();
@@ -34,6 +67,29 @@ public class AdminClientWrapper {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void topicsListing(String configFile) {
+        try (AdminClient client = AdminClient.create(loadConfig(configFile))) {
+            ListTopicsResult listTopicsResult = client.listTopics();
+            KafkaFuture<Collection<TopicListing>> listings = listTopicsResult.listings();
+            Collection<TopicListing> topicListings = listings.get();
+            Iterator iterator = topicListings.iterator();
+            while ( iterator.hasNext()){
+                TopicListing topicListing = (TopicListing)iterator.next();
+                System.out.printf("Name:%s-Internal?%s\n", topicListing.name(), topicListing.isInternal()?"true":"false");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void mainMenu(String[] options){
+        for (String option : options){
+            System.out.println(option);
+        }
+        System.out.print("Choose operation: ");
     }
 
 
