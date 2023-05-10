471438a61702002c2880152215905813e209bc0c
hifly81 <gio.marigi@gmail.com>
2023-05-10 14:11:16 +0200

    asepctj demo#10

--- kafka-smt-aspectj/src/main/java/org/hifly/kafka/smt/aspectj/SMTAspect.java
+++ kafka-smt-aspectj/src/main/java/org/hifly/kafka/smt/aspectj/SMTAspect.java
@@ -9,11 +9,14 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class SMTAspect {
 
-    @Pointcut("execution(* org.apache.kafka.connect.transforms.Filter.apply(..))")
+    @Pointcut("execution(* org.apache.kafka.connect.transforms.*.apply(..)) && !execution(* org.apache.kafka.connect.runtime.PredicatedTransformation.apply(..))")
     public void standardMethod() {}
 
     @Before("standardMethod()")
     public void log(JoinPoint jp) throws Throwable {
+
+        System.out.println(jp);
+
         Object[] array = jp.getArgs();
         if(array != null) {
             System.out.println("Size:" + array.length);
