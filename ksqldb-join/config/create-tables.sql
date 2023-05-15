b23df8176a7f0970847fc4bae5811e9b8290580f
hifly81 <gio.marigi@gmail.com>
2023-05-15 16:57:18 +0200

    Fix #23

--- ksqldb-join/config/create-tables.sql
+++ ksqldb-join/config/create-tables.sql
@@ -1,8 +1,18 @@
+CREATE TABLE maintenance (
+	id serial PRIMARY KEY,
+	maintenance VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+
 CREATE TABLE device (
 	id serial PRIMARY KEY,
 	fullname VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
 
+
+insert into maintenance (id, maintenance) values (1, '2023-03-01 15:00:00 16:00:00');
+
 insert into device (id, fullname) values (1, 'foo11111');
 insert into device (id, fullname) values (2, 'foo22222');
+insert into device (id, fullname) values (10, 'foo1010101010');
+insert into device (id, fullname) values (15, 'foo1515151515');
---
+++ ksqldb-join/ksql/ksql-statements-rj.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tr '\n' ' ' < statements-rj.sql | \
+sed 's/;/;\'$'\n''/g' | \
+while read stmt; do
+    echo '{"ksql":"'$stmt'", "streamsProperties": {}}' | \
+        curl -s -X "POST" "http://ksqldb-server:8088/ksql" \
+             -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
+             -d @- | \
+        jq
+done
