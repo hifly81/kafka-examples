f6a0a7328ed015bd7d24510f0294c82da64bd898
Giovanni Marigi <hifly81@m2kqyq4w1k.homenet.telecomitalia.it>
2023-10-06 13:03:43 +0200

    retry handle per message

--- kafka-consumer-retry-topics/src/main/java/org/hifly/kafka/demo/consumer/retry/RetryHandle.java
+++ kafka-consumer-retry-topics/src/main/java/org/hifly/kafka/demo/consumer/retry/RetryHandle.java
@@ -10,12 +10,14 @@ import org.hifly.kafka.demo.producer.ProducerCallback;
 import org.hifly.kafka.demo.producer.serializer.string.StringProducer;
 
 import java.math.BigInteger;
-import java.util.List;
+import java.util.*;
 
 public class RetryHandle<K,V> extends ConsumerHandle<K,V> {
 
     private final String RETRY_TOPIC = "retry_topic";
     private final String DLQ_TOPIC = "dlq_topic";
+
+    private Map<String, Integer> retriesPerKey = new HashMap<>();
 
     private int retries;
 
@@ -30,20 +32,28 @@ public class RetryHandle<K,V> extends ConsumerHandle<K,V> {
     @Override
     public void process(ConsumerRecords<K, V> consumerRecords, String groupId, String consumerId) {
         for (ConsumerRecord<K, V> record : consumerRecords) {
-            ConsumerRecordUtil.prettyPrinter(groupId,  consumerId, record);
+            ConsumerRecordUtil.prettyPrinter(groupId, consumerId, record);
             for (Header recordHeader : record.headers()) {
+                //This is only a sample way to detect if a record should be a candidate for retries. Real use-case will fail on same business logic
                 if (recordHeader.key().equals("ERROR")) {
                     System.out.printf("Error message detected: number of retries left %s\n", retries);
-                }
-                if(this.retries > 0) {
-                    System.out.printf("send to RETRY topic: %s\n", RETRY_TOPIC);
-                    //move to retry topic
-                    //record.headers().add("RETRY", BigInteger.valueOf(retries).toByteArray());
-                    this.stringProducer.produceAsync((ProducerRecord<String, String>) new ProducerRecord<>(RETRY_TOPIC, record.key(),record.value()), new ProducerCallback());
-                    this.retries--;
-                } else {
-                    System.out.printf("number of retries exhausted, send to DLQ topic: %s\n", DLQ_TOPIC);
-                    this.stringProducer.produceAsync((ProducerRecord<String, String>) new ProducerRecord<>(DLQ_TOPIC, record.key(),record.value()), new ProducerCallback());
+                    if (retriesPerKey.containsKey(record.key())) {
+                        Integer retryValue = retriesPerKey.get(record.key());
+                        if (retryValue > 0) {
+                            System.out.printf("send to RETRY topic: %s\n", RETRY_TOPIC);
+                            //move to retry topic
+                            this.stringProducer.produceAsync(
+                                    (ProducerRecord<String, String>) new ProducerRecord<>(RETRY_TOPIC, record.key(), record.value()), new ProducerCallback());
+                            retryValue = retryValue - 1;
+                            retriesPerKey.put((String) record.key(), retryValue);
+                        } else {
+                            System.out.printf("number of retries exhausted, send to DLQ topic: %s\n", DLQ_TOPIC);
+                            this.stringProducer.produceAsync(
+                                    (ProducerRecord<String, String>) new ProducerRecord<>(DLQ_TOPIC, record.key(), record.value()), new ProducerCallback());
+                        }
+                    } else {
+                        retriesPerKey.put((String) record.key(), retries);
+                    }
                 }
 
             }
