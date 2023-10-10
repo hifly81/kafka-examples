b4900dabb5df5893d9d810b8ecf7ffa19e263b2f
hifly81 <gio.marigi@gmail.com>
2023-10-10 14:19:07 +0200

    fixed StringProducer

--- kafka-producer/src/main/java/org/hifly/kafka/demo/producer/RecordMetadataUtil.java
+++ kafka-producer/src/main/java/org/hifly/kafka/demo/producer/RecordMetadataUtil.java
@@ -1,16 +1,12 @@
 package org.hifly.kafka.demo.producer;
 
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RecordMetadataUtil {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RecordMetadataUtil.class);
-
     public static void prettyPrinter(RecordMetadata recordMetadata) {
         if(recordMetadata != null) {
-            LOGGER.info("Topic: {} - Partition: {} - Offset: {}\n",
+            System.out.printf("Topic: %s - Partition: %d - Offset: %d\n",
                     recordMetadata.topic(),
                     recordMetadata.partition(),
                     recordMetadata.offset());
--- kafka-producer/src/main/java/org/hifly/kafka/demo/producer/serializer/string/Runner.java
+++ kafka-producer/src/main/java/org/hifly/kafka/demo/producer/serializer/string/Runner.java
@@ -9,26 +9,32 @@ public class Runner {
 
     public static void main (String [] args) throws Exception {
         StringProducer baseProducer = new StringProducer();
-        baseProducer.start(null);
+        baseProducer.start();
         bunchOfSynchMessages("topic1", baseProducer);
+        baseProducer.start();
         bunchOfFFMessages("topic1", baseProducer);
+        baseProducer.start();
         bunchOfAsynchMessages("topic1", baseProducer);
     }
 
     public static void bunchOfSynchMessages(String topic, StringProducer baseProducer) {
         RecordMetadata lastRecord = null;
-        for (int i= 10; i < 30000; i++ )
+        for (int i= 10; i < 100; i++ ) {
             lastRecord = baseProducer.produceSync(new ProducerRecord<>(topic, Integer.toString(i)));
-        RecordMetadataUtil.prettyPrinter(lastRecord);
+            RecordMetadataUtil.prettyPrinter(lastRecord);
+        }
+        baseProducer.stop();
     }
 
     public static void bunchOfFFMessages(String topic, StringProducer baseProducer) {
-        for (int i= 10; i < 30000; i++ )
+        for (int i= 10; i < 100; i++ )
              baseProducer.produceFireAndForget(new ProducerRecord<>(topic, Integer.toString(i)));
+        baseProducer.stop();
     }
 
     public static void bunchOfAsynchMessages(String topic, StringProducer baseProducer) {
-        for (int i= 10; i < 30000; i++ )
+        for (int i= 10; i < 100; i++ )
             baseProducer.produceAsync(new ProducerRecord<>(topic, Integer.toString(i)), new ProducerCallback());
+        baseProducer.stop();
     }
 }
