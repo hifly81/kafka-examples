947657842961dc4284d119bab58252cd39951eda
hifly81 <gio.marigi@gmail.com>
2023-10-14 10:07:11 +0200

    remove spring apps from parent pom

---
+++ release/build.sh
@@ -0,0 +1,34 @@
+POM_VERSION=1.2.1
+KAFKA_VERSION=3.5.1
+CFLT_VERSION=7.5.1
+
+PRJ_HOME=..
+SPRING_PRODUCER_HOME=../kafka-springboot-producer
+SPRING_CONSUMER_HOME=../kafka-springboot-consumer
+
+
+function compile {
+  printf "\n\nCompiling..\n\n"
+  mvn -f $1/pom.xml clean compile
+}
+
+function release {
+  printf "\n\nCompiling..\n\n"
+  mvn -f $1/pom.xml clean compile
+
+  printf "\n\nRun tests..\n\n"
+  mvn -f $1/pom.xml clean test
+
+  printf "\n\nPackaging..\n\n"
+  mvn -f $1/pom.xml clean install
+}
+
+if [ "$1" == "compile" ]; then
+  compile $PRJ_HOME
+  compile $SPRING_PRODUCER_HOME
+  compile $SPRING_CONSUMER_HOME
+elif [ "$1" == "release" ]; then
+  release $PRJ_HOME
+  release $SPRING_PRODUCER_HOME
+  release $SPRING_CONSUMER_HOME
+fi
