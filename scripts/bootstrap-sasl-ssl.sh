42b0244a3eafcd5651cd366ced94caa884837880
hifly81 <gio.marigi@gmail.com>
2024-02-26 18:14:05 +0100

    sasl ssl listener with example

--- scripts/bootstrap-sasl-ssl.sh
+++ scripts/bootstrap-sasl-ssl.sh
@@ -14,13 +14,11 @@ mkdir -p $SASL_SSL_SECRETS_FOLDER
 
 cp sasl-ssl/config/broker_jaas.conf $SASL_SSL_SECRETS_FOLDER
 
-# Generate CA key
-openssl req -new -x509 -keyout $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.key -out $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
+openssl req -new -x509 -keyout $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.key -out $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.hifly.org/OU=Hifly/O=Hifly/L=Rome/ST=Lazio/C=IT' -passin pass:confluent -passout pass:confluent
 
-# Create host keystore
 keytool -genkey -noprompt \
   -alias broker \
-  -dname "CN=broker,OU=TEST,O=CONFLUENT,L=PaloAlto,S=Ca,C=US" \
+  -dname "CN=broker,OU=Hifly,O=Hifly,L=Rome,S=Lazio,C=IT" \
   -ext "SAN=dns:broker,dns:localhost" \
   -keystore $SASL_SSL_SECRETS_FOLDER/kafka.broker.keystore.jks \
   -keyalg RSA \
@@ -28,7 +26,6 @@ keytool -genkey -noprompt \
   -keypass confluent \
   -storetype pkcs12
 
-# Create the certificate signing request (CSR)
 keytool -keystore $SASL_SSL_SECRETS_FOLDER/kafka.broker.keystore.jks -alias broker -certreq -file $SASL_SSL_SECRETS_FOLDER/broker.csr -storepass confluent -keypass confluent -ext "SAN=dns:broker,dns:localhost"
 
 cat << EOF > $SASL_SSL_SECRETS_FOLDER/extfile
@@ -45,22 +42,26 @@ DNS.1 = broker
 DNS.2 = localhost
 EOF
 
-# Sign the host certificate with the certificate authority (CA)
 openssl x509 -req -CA $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -CAkey $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.key -in $SASL_SSL_SECRETS_FOLDER/broker.csr -out $SASL_SSL_SECRETS_FOLDER/broker-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent -extensions v3_req -extfile $SASL_SSL_SECRETS_FOLDER/extfile
 
-# Sign and import the CA cert into the keystore
 keytool -noprompt -keystore $SASL_SSL_SECRETS_FOLDER/kafka.broker.keystore.jks -alias CARoot -import -file $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
-# Sign and import the host certificate into the keystore
 keytool -noprompt -keystore $SASL_SSL_SECRETS_FOLDER/kafka.broker.keystore.jks -alias broker -import -file $SASL_SSL_SECRETS_FOLDER/broker-ca1-signed.crt -storepass confluent -keypass confluent -ext "SAN=dns:broker,dns:localhost"
 
-# Create truststore and import the CA cert
 keytool -noprompt -keystore $SASL_SSL_SECRETS_FOLDER/kafka.broker.truststore.jks -alias CARoot -import -file $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
-# Save creds
 echo  "confluent" > $SASL_SSL_SECRETS_FOLDER/broker_sslkey_creds
 echo  "confluent" > $SASL_SSL_SECRETS_FOLDER/broker_keystore_creds
 echo  "confluent" > $SASL_SSL_SECRETS_FOLDER/broker_truststore_creds
+
+
+keytool -keystore $SASL_SSL_SECRETS_FOLDER/kafka.client.truststore.jks -alias CARoot -storepass confluent -importcert -file $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -noprompt
+keytool -keystore $SASL_SSL_SECRETS_FOLDER/kafka.client.keystore.jks -alias kafka-client -validity 365 -genkey -keyalg RSA -storepass confluent -keypass confluent -dname "CN=client,OU=Hifly,O=Hifly,L=Rome,ST=Lazio,C=IT" -ext "SAN=dns:client,dns:localhost"
+keytool -keystore $SASL_SSL_SECRETS_FOLDER/kafka.client.keystore.jks -alias CARoot -storepass confluent -importcert -file $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -noprompt
+keytool -keystore $SASL_SSL_SECRETS_FOLDER/kafka.client.keystore.jks -alias kafka-client -storepass confluent -certreq -file $SASL_SSL_SECRETS_FOLDER/kafka-client-csr -ext "SAN=dns:client,dns:localhost"
+openssl x509 -req -CA $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.crt -CAkey $SASL_SSL_SECRETS_FOLDER/snakeoil-ca-1.key -in $SASL_SSL_SECRETS_FOLDER/kafka-client-csr -out $SASL_SSL_SECRETS_FOLDER/kafka-client-cer -days 365 -CAcreateserial -passin pass:confluent
+keytool -keystore $SASL_SSL_SECRETS_FOLDER/kafka.client.keystore.jks -alias kafka-client -storepass confluent -importcert -file $SASL_SSL_SECRETS_FOLDER/kafka-client-cer -noprompt -ext "SAN=dns:client,dns:localhost"
+
 
 echo "Starting Kafka cluster..."
 docker-compose -f sasl-ssl/docker-compose.yml --env-file .env up -d
