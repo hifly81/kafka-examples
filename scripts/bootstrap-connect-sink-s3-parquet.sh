641dc54fcfbb20343c84c34d5c0ae4f4fc4193b9
hifly81 <gio.marigi@gmail.com>
2024-03-20 12:36:40 +0100

    added example with parquet

---
+++ scripts/bootstrap-connect-sink-s3-parquet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sh scripts/tear-down-connect-sink-s3.sh
+
+echo "Starting Kafka cluster..."
+docker-compose -f kafka-connect-sink-s3/docker-compose.yml --env-file .env up -d
+
+echo "Wait 20 seconds..."
+
+sleep 20
+
+echo "Create gaming-player-activity..."
+docker exec -it broker kafka-topics --bootstrap-server broker:9092 --create --topic gaming-player-activity --replication-factor 1 --partitions 1
+
+echo "Installing s3 parquet format sink..."
+curl -X POST -H Accept:application/json -H Content-Type:application/json http://localhost:8083/connectors/ -d @kafka-connect-sink-s3/config/s3_parquet_sink.json
+
+echo "Wait 3 seconds..."
+
+sleep 3
+
+echo "connectors status..."
+curl -v http://localhost:8083/connectors?expand=status
