1afb00c936d5238375319a9d66247c76cb114203
hifly81 <gio.marigi@gmail.com>
2024-08-06 06:47:23 -0700

    upgrade to kafka 3.8

--- performance/trogdor/coordinator/compression/nocompression/trogdor-task-cancel.sh
+++ performance/trogdor/coordinator/compression/nocompression/trogdor-task-cancel.sh
@@ -8,6 +8,6 @@ increment=1
 current=$start
 
 while(( $(echo "$current < $end" | /usr/bin/bc -l) )); do
-  /kafka_2.13-3.7.1/bin/trogdor.sh client destroyTask -t localhost:8889 -i node-$current
+  /kafka_2.13-3.8.0/bin/trogdor.sh client destroyTask -t localhost:8889 -i node-$current
   current=$(echo "$current + $increment" | /usr/bin/bc -l)
 done
