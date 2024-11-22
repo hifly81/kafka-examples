440768174e3e020e0733472cef441159c26a89f2
hifly81 <gio.marigi@gmail.com>
2024-11-22 04:59:08 -0800

    added kstream bounded mem 2

--- scripts/tear-down-connect-postgres-to-mongo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Stopping docker containers..."
-docker-compose -f kafka-connect-source-event-router/docker-compose.yml --env-file .env down --volumes
-rm -Rf kafka-connect-source-event-router/postgres-data
