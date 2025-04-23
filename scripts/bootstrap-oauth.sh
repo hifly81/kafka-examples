aeb03df0e1681d10349a3058d3d4c69c6bba803d
hifly81 <gio.marigi@gmail.com>
2025-04-23 01:40:11 -0700

    removed zk from oauth example

--- scripts/bootstrap-oauth.sh
+++ scripts/bootstrap-oauth.sh
@@ -4,8 +4,3 @@ sh scripts/tear-down.sh
 
 echo "Starting IDP... (wait 15 seconds)"
 docker-compose -f kafka-oauth-kip-768/docker-compose-idp.yml --env-file .env up -d
-
-sleep 15
-
-echo "Starting Kafka cluster..."
-docker-compose -f kafka-oauth-kip-768/docker-compose-oauth.yml --env-file .env up -d
