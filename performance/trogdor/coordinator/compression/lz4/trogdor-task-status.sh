b86e7a879165ed7795b99526cb7945c4bf773d97
hifly81 <gio.marigi@gmail.com>
2025-09-30 12:40:52 -0700

    upgrade to kafka 4.1.0

--- performance/trogdor/coordinator/compression/lz4/trogdor-task-status.sh
+++ performance/trogdor/coordinator/compression/lz4/trogdor-task-status.sh
@@ -8,6 +8,6 @@ increment=1
 current=$start
 
 while(( $(echo "$current < $end" | /usr/bin/bc -l) )); do
-  /kafka_2.13-4.0.0/bin/trogdor.sh client showTask -t localhost:8889 -i node-$current --show-status
+  /kafka_2.13-4.1.0/bin/trogdor.sh client showTask -t localhost:8889 -i node-$current --show-status
   current=$(echo "$current + $increment" | /usr/bin/bc -l)
 done
--- performance/trogdor/coordinator/trogdor-coordinator.sh
+++ performance/trogdor/coordinator/trogdor-coordinator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -x
-/kafka_2.13-4.0.0/bin/trogdor.sh coordinator -c /tmp/trogdor/coordinator/trogdor-coordinator.conf -n node0 >/tmp/trogdor/coordinator/trogdor-coordinator.log 2>&1 &
+/kafka_2.13-4.1.0/bin/trogdor.sh coordinator -c /tmp/trogdor/coordinator/trogdor-coordinator.conf -n node0 >/tmp/trogdor/coordinator/trogdor-coordinator.log 2>&1 &
